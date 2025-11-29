d41097aa0ad072b66738094487cbf39a9e309840
Caroline Grand-Clement <187895937+caroloading@users.noreply.github.com>
2025-11-29 10:16:50 +0100

    add visuals

--- src/ui.ts
+++ src/ui.ts
@@ -2,8 +2,14 @@ import { setupButton } from "./isu";
 
 document.querySelector("#app")!.innerHTML = `
   <div>
+    <h1>SeaMetrics</h1>
+    <h3>A dialogue system to sail the day!</h3>
     <div class="card">
       <button id="counter" type="button"></button>
+    </div>
+    <img src="seametrics_logo-transparent.png" alt="SeaMetrics logo" style="width:300px;height:200px;">
+    <div class="footer">
+      <p>Developed by Caroline Grand-Clement for GU's LT2319 Dialogsystem 2 - HT 2025.</p>
     </div>
   </div>
 `;
